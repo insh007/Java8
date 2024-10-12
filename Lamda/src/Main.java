4d578fe4f1a84997417095ab74a0649db93e0931
insh007 <aliinshad007@gmail.com>
2024-10-13 03:25:02 +0530

    some different way use

--- Lamda/src/Main.java
+++ Lamda/src/Main.java
@@ -32,14 +32,14 @@ public class Main {
 
         Runner runner = new Runner();
         // Pass as an Anonymous class - Before Java 8
-        runner.run(new Executable() {
-            @Override
-            public int execute(int a, int b) {
-                System.out.println("Hello there!!");
-                int d = 50; //can do this in method of anonymous classes because of other scope in anonymous class
-                return c + d + b + a; // 160 - 100+50+5+5
-            }
-        });
+//        runner.run(new Executable() {
+//            @Override
+//            public int execute(int a, int b) {
+//                System.out.println("Hello there!!");
+//                int d = 50; //can do this in method of anonymous classes because of other scope in anonymous class
+//                return c + d + b + a; // 160 - 100+50+5+5
+//            }
+//        });
 
         System.out.println("================================");
         // Using Lambda Expression
@@ -53,15 +53,21 @@ public class Main {
 //        });
 
         // We don't need curly bracket if there is only single expression
-        runner.run((a,b) -> {
-//            int d = 50; // can't do this because of already defined variable; no new scope
+//        runner.run((a,b) -> {
+////            int d = 50; // can't do this because of already defined variable; no new scope
+//           return c + d + b + a; // 150 - 100+40+5+5
+//        });
+
+        System.out.println("some different use");
+        Executable ex = (a,b) -> {
            return c + d + b + a; // 150 - 100+40+5+5
-        });
+        };
 
-//        System.out.println("Method Reference");
-//        runner.run(Integer::sum);
-//        runner.run(a,b -> 10 + a); // without use of bracket this will not work with multiple parameters
+        runner.run(ex);
 
+        Object codeBlock = (Executable)(a,b) -> {
+            return c + d + b + a; // 150 - 100+40+5+5
+        };
 
     }
 }
