3e6772991c2ff342e18f8927bc588358ae3e49b8
insh007 <aliinshad007@gmail.com>
2024-10-13 02:31:53 +0530

    when interface method has parameter

--- Lamda/src/Main.java
+++ Lamda/src/Main.java
@@ -1,12 +1,12 @@
 // Functional Interface
 interface Executable {
-    int execute(); // abstract method
+    int execute(int a); // abstract method
 }
 
 class Runner {
     public void run(Executable e){
         System.out.println("Executing code block....");
-        int value = e.execute();
+        int value = e.execute(5);
         System.out.println("Return value is: " + value);
     }
 }
@@ -17,9 +17,9 @@ public class Main {
         // Pass as an Anonymous class - Before Java 8
         runner.run(new Executable() {
             @Override
-            public int execute() {
+            public int execute(int a) {
                 System.out.println("Hello there!!");
-                return 7;
+                return 7 + a;
             }
         });
 
@@ -28,13 +28,15 @@ public class Main {
 //        runner.run(() -> System.out.println("Hello there!! - Lambda"));
 
         // Lambda with multiple statements
-        runner.run(() -> {
-            System.out.println("One more statement with the help of Lambda");
-            System.out.println("Hello there!! - Lambda");
-            return 8;
-        });
+//        runner.run(() -> {
+//            System.out.println("One more statement with the help of Lambda");
+//            System.out.println("Hello there!! - Lambda");
+//            return 8;
+//        });
 
         // We don't need curly bracket if there is only single expression
-        runner.run(() -> 10);
+        runner.run((int a) -> 10 + a);
+        // OR
+        runner.run((a) -> 10 + a);
     }
 }
