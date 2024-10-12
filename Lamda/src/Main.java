a97911e8caa8bc04e5445f9cca807c6702b247af
insh007 <aliinshad007@gmail.com>
2024-10-13 01:45:44 +0530

    before Java8 - Anonymous class was used

--- Lamda/src/Main.java
+++ Lamda/src/Main.java
@@ -1,5 +1,25 @@
+interface Executable {
+    void execute();
+}
+
+class Runner {
+    public void run(Executable e){
+        System.out.println("Executing code block....");
+        e.execute();
+
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+        Runner runner = new Runner();
+        // Pass as an Anonymous class - Before Java 8
+        runner.run(new Executable() {
+            @Override
+            public void execute() {
+                System.out.println("Hello there!!");
+            }
+        });
 
     }
 }
